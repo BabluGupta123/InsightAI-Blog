201bca01a4c49d71e2de1f98e5d0e7b5925cbb09
BabluGupta123 <bablu8795379783@gmail.com>
2025-07-18 08:29:35 +0530

    fix: corrected import case for Home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Blog from "./pages/Blog";
 import Layout from "./pages/admin/Layout";
 import Dashboard from "./pages/admin/Dashboard";
